bd2d4c46ede7d1cff9518a28a1eda136cb42bad2
Rushikesh Athare <rushikeshathare02@gmail.com>
2024-08-08 23:26:00 +0530

    Update MongoDB connection to use environment variables

--- mongodb_connection.py
+++ mongodb_connection.py
@@ -1,19 +1,26 @@
-# mongodb_connection.py
 from pymongo import MongoClient, errors
+import os
 
-# Define your MongoDB URI
-MONGO_URI = "mongodb+srv://Mayur:Mayur2002@cluster0.cemqecf.mongodb.net/InterShipPortal?retryWrites=true&w=majority&appName=Cluster0"
+# Fetch MongoDB URI from environment variables
+MONGO_URI = os.getenv("mongodb+srv://Mayur:Mayur2002@cluster0.cemqecf.mongodb.net/InterShipPortal?retryWrites=true&w=majority&appName=Cluster0"
+)
 
 def get_db():
     """
     Connects to MongoDB and returns the database object.
     """
     try:
+        if MONGO_URI is None:
+            raise ValueError("MONGO_URI environment variable not set")
+
         client = MongoClient(MONGO_URI)
         client.admin.command('ping')  # Check the connection
         print("Database connected successfully.")
         db = client['InterShipPortal']
         return db
+    except ValueError as ve:
+        print(ve)
+        return None
     except errors.ServerSelectionTimeoutError as err:
         print(f"Database connection failed: {err}")
         return None
